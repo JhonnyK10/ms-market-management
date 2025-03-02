a7348fc800a3b42a44766a3be4b86a0618e18f65
JhonnyK10 <jhonnylino@gmail.com>
2025-03-02 00:26:21 -0300

    base of controller/service/model/infra

---
+++ src/Application/Service/user_service.py
@@ -0,0 +1,7 @@
+from Domain.user import create_user
+from Infrastructure.Models.user import save_user_model
+
+def save(usuario):
+    user = create_user(usuario)
+    save_user_model(user)
+    return
